626c53de31555d32e1470cbdc6beb694f6a6f4be
DELL-PC <veena.spandana08@gmail.com>
2026-02-12 16:37:45 +0530

    Uodated CustomerController class

--- app-crud/src/main/java/com/bank/app/controller/CustomerController.java
+++ app-crud/src/main/java/com/bank/app/controller/CustomerController.java
@@ -2,47 +2,62 @@ package com.bank.app.controller;
 
 import com.bank.app.model.Customer;
 import com.bank.app.service.CustomerService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/customers")
 public class CustomerController {
 
-    @Autowired
-    private CustomerService customerService;
+    private final CustomerService customerService;
 
-    @PostMapping("/api/customers")
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping
     public ResponseEntity<String> createCustomer(@RequestBody Customer customer){ // Accepts a JSON request to create a new
                                                                         // customer and returns the created customer.
         customerService.createCustomer(customer);
         return new ResponseEntity<>("Customer created successfully!",HttpStatus.CREATED);
+//         return ResponseEntity.ok(customerService.createCustomer(customer));
 
     }
 
-    @GetMapping("/api/customers")
-    public void getAllCustomers() { // Returns a list of all customers.
-        customerService.getAllCustomers();
+    @GetMapping
+    public ResponseEntity<List<Customer>> getAllCustomers() { // Returns a list of all customers.
+        return ResponseEntity.ok(customerService.getAllCustomers()) ;
     }
-    @GetMapping("/api/customers/{id}")
-    public void getCustomerById(@PathVariable Long id) { // Returns a single customer by ID.
-        customerService.getCustomerById(id);
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Customer>> getCustomerById(@PathVariable Long id) { // Returns a single customer by ID.
+        return ResponseEntity.ok(customerService.getCustomerById(id));
 
-    }
-
-    @PutMapping("/api/customers/{id}")
-    public void updateCustomer(@RequestBody Customer customer, @PathVariable Long id) { // Updates an existing customer’s details and returns the updated customer.
-//        Customer updatedData = customerService.updateCustomer(customer,id);
-//        String status =
+//        return customerService.getCustomerById(id)
+//                .map(ResponseEntity::ok)
+//                .orElse(ResponseEntity.notFound().build());
 
     }
 
-    @DeleteMapping("/api/customers/{id}")
-    public void deleteCustomer(@PathVariable Long id) { // Deletes a customer and returns a confirmation message.
+    @PutMapping("/{id}")
+    public ResponseEntity<Customer> updateCustomer(@RequestBody Customer customer, @PathVariable Long id) { // Updates an existing customer’s details and returns the updated customer.
+        try{
+            return ResponseEntity.ok(customerService.updateCustomer(customer, id));
+        }catch(RuntimeException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCustomer(@PathVariable Long id) { // Deletes a customer and returns a confirmation message.
+        try{
+            customerService.deleteCustomer(id);
+            return ResponseEntity.ok("Customer deleted successfully");
+        }catch(RuntimeException e){
+            return ResponseEntity.notFound().build();
+        }
     }
 }
