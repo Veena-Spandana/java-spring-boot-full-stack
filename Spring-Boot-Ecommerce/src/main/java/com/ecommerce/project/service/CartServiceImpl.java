f33201347149214fea687fc4f4e317fcf556f73d
DELL-PC <veena.spandana08@gmail.com>
2026-03-27 21:02:51 +0530

    added some files and updated some

---
+++ Spring-Boot-Ecommerce/src/main/java/com/ecommerce/project/service/CartServiceImpl.java
@@ -0,0 +1,129 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.exceptions.APIException;
+import com.ecommerce.project.exceptions.ResourceNotFoundException;
+import com.ecommerce.project.model.Cart;
+import com.ecommerce.project.model.CartItem;
+import com.ecommerce.project.model.Product;
+import com.ecommerce.project.payload.CartDTO;
+import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.repositories.CartItemRepository;
+import com.ecommerce.project.repositories.CartRepository;
+import com.ecommerce.project.repositories.ProductRepository;
+import com.ecommerce.project.util.AuthUtil;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+@Service
+public class CartServiceImpl implements CartService{
+
+    @Autowired
+    CartRepository cartRepository;
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Autowired
+    CartItemRepository cartItemRepository;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Autowired
+    AuthUtil authUtil;
+
+    @Override
+    public CartDTO addProductToCart(Long productId, Integer quantity) {
+
+        // Find existing cart or create one
+        Cart cart = createCart();
+
+        // Retrieve Product Details
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new ResourceNotFoundException("Product", "productId", productId));
+
+        // Perform Validations
+        CartItem cartItem = cartItemRepository.findCartItemByProductIdAndCartId(
+                cart.getCartId(),
+                productId
+        );
+
+        if(cartItem != null){
+            throw new APIException("Product " + product.getProductName() + "already exists in the cart");
+        }
+
+        if(product.getQuantity() == 0){
+            throw new APIException(product.getProductName() + " is not available");
+        }
+
+        if(product.getQuantity() < quantity){
+            throw new APIException("Please, make an order of the " + product.getProductName()
+            + " less than or equal to the quantity " + product.getQuantity() + ".");
+        }
+
+        // Create Cart Item
+        CartItem newCartItem = new CartItem();
+        newCartItem.setProduct(product);
+        newCartItem.setCart(cart);
+        newCartItem.setQuantity(quantity);
+        newCartItem.setDiscount(product.getDiscount());
+        newCartItem.setProductPrice(product.getSpecialPrice());
+
+        // Save Cart Item
+        cartItemRepository.save(newCartItem);
+
+        product.setQuantity(product.getQuantity());
+
+        cart.setTotalPrice(cart.getTotalPrice() + (product.getSpecialPrice() * quantity));
+
+        // Save updated cart details
+        cartRepository.save(cart);
+
+        // Return updated cart
+
+        CartDTO cartDTO = modelMapper.map(cart, CartDTO.class);
+
+        // Product doesn't reflect/know/store how many items user has added to the Cart - this information/quantity exists in Cart Item
+        // So we need make use of Cart Item to get that quantity
+
+        // First map the Product to ProductDTO
+        // Then set the quantity from Cart Item and then add this List to CartDTO
+
+        // Why we are willing to get the list of ProductDTO? - so that we can update it in CartDTO
+
+        List<CartItem> cartItems = cart.getCartItems();
+
+        Stream<ProductDTO> productStream = cartItems.stream().map(item -> {
+            ProductDTO map = modelMapper.map(item.getProduct(), ProductDTO.class);
+            map.setQuantity(item.getQuantity()); // This quantity is updated with quantity from Cart Item
+                                                // If we don't do this step explicitly ,
+                                                // the quantity that is existed in the Product will be assigned in the ProductDTO
+
+            // This quantity is representing what quantity of the Product that is added in the Cart
+            return map;
+        });
+
+        cartDTO.setProducts(productStream.toList());
+
+        return cartDTO;
+    }
+
+    private Cart createCart() {
+
+        // Find existing cart or create one
+        Cart userCart = cartRepository.findCartByEmail(authUtil.loggedInEmail());
+        if(userCart != null){
+            return userCart;
+        }
+
+        Cart cart = new Cart();
+        cart.setTotalPrice(0.00);
+        cart.setUser(authUtil.loggedInUser());
+        Cart newCart = cartRepository.save(cart);
+        return newCart;
+    }
+}
