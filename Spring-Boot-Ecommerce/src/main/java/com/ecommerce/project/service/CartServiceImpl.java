f3bab76d7fa8df7ab4791e28a4a609021eb29a90
DELL-PC <veena.spandana08@gmail.com>
2026-04-04 14:35:51 +0530

    code is updated for some classes

--- Spring-Boot-Ecommerce/src/main/java/com/ecommerce/project/service/CartServiceImpl.java
+++ Spring-Boot-Ecommerce/src/main/java/com/ecommerce/project/service/CartServiceImpl.java
@@ -208,12 +208,23 @@ public class CartServiceImpl implements CartService{
             throw new APIException("Product " + product.getProductName() + " not available in the cart");
         }
 
-        cartItem.setProductPrice(product.getSpecialPrice());
-        cartItem.setQuantity(cartItem.getQuantity() + quantity);
-        cartItem.setDiscount(product.getDiscount());
-        cart.setTotalPrice(cart.getTotalPrice() + cartItem.getProductPrice() * quantity);
-        cartRepository.save(cart);
+        // Calculate new quantity
+        int newQuantity = cartItem.getQuantity() + quantity;
 
+        // Validation to prevent negative quantities
+        if(newQuantity < 0){
+            throw new APIException("The resulting quantity cannot be negative.");
+        }
+
+        if(newQuantity == 0){
+            deleteProductFromCart(cartId, productId);
+        } else {
+            cartItem.setProductPrice(product.getSpecialPrice());
+            cartItem.setQuantity(cartItem.getQuantity() + quantity);
+            cartItem.setDiscount(product.getDiscount());
+            cart.setTotalPrice(cart.getTotalPrice() + cartItem.getProductPrice() * quantity);
+            cartRepository.save(cart);
+        }
         CartItem updatedItem = cartItemRepository.save(cartItem);
         if(cartItem.getQuantity() == 0){
             cartItemRepository.deleteById(updatedItem.getCartItemId());
@@ -234,6 +245,7 @@ public class CartServiceImpl implements CartService{
         return cartDTO;
     }
 
+    @Transactional
     @Override
     public String deleteProductFromCart(Long cartId, Long productId) {
         Cart cart = cartRepository.findById(cartId)
@@ -241,8 +253,14 @@ public class CartServiceImpl implements CartService{
 
         CartItem cartItem = cartItemRepository.findCartItemByProductIdAndCartId(cartId, productId);
         if(cartItem == null){
-            throw new ResourceNotFoundException("Product", "");
+            throw new ResourceNotFoundException("Product", "productId", productId);
         }
-        return "";
+
+        cart.setTotalPrice(cart.getTotalPrice() -
+                (cartItem.getProductPrice() * cartItem.getQuantity()));
+
+        cartItemRepository.deleteCartItemByProductIdAndCartId(cartId, productId);
+
+        return "Product " + cartItem.getProduct().getProductName() + " removed from the cart !!!";
     }
 }
