e6cd6fc649ac5fb3a4439ab625188ca69b54cd14
DELL-PC <veena.spandana08@gmail.com>
2026-04-08 15:39:23 +0530

    updated some files

--- Spring-Boot-Ecommerce/src/main/java/com/ecommerce/project/service/AddressServiceImpl.java
+++ Spring-Boot-Ecommerce/src/main/java/com/ecommerce/project/service/AddressServiceImpl.java
@@ -1,11 +1,14 @@
 package com.ecommerce.project.service;
 
+import com.ecommerce.project.model.Address;
 import com.ecommerce.project.model.User;
 import com.ecommerce.project.payload.AddressDTO;
 import com.ecommerce.project.repositories.AddressRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AddressServiceImpl implements AddressService{
@@ -18,7 +21,17 @@ public class AddressServiceImpl implements AddressService{
 
     @Override
     public AddressDTO createAddress(AddressDTO addressDTO, User user) {
-        return null;
+
+        Address address = modelMapper.map(addressDTO, Address.class);
+
+        List<Address> addressList = user.getAddresses();
+        addressList.add(address);
+        user.setAddresses(addressList);
+
+        address.setUser(user);
+        Address savedAddress = addressRepository.save(address);
+
+        return modelMapper.map(savedAddress, AddressDTO.class);
 
     }
 }
